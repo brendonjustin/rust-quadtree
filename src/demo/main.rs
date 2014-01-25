08abc01f9c497ef7fc741abe7705f6ed8e3b8917
Brendon Justin <brendonjustin@gmail.com>
2014-01-25 15:57:42 -0600

    Use spaces instead of tabs.

--- src/demo/main.rs
+++ src/demo/main.rs
@@ -1,8 +1,8 @@
 extern mod quadtree;
 
 fn main() {
-	let origin = quadtree::Point { 0 as f32, 0 as f32 };
-	let size = quadtree::Size { 1 as f32, 1 as f32 };
-	let rect = quadtree::Rect::new(origin, size);
-	let qt = quadtree::QuadTree::newAutosized(rect);
+    let origin = quadtree::Point { 0 as f32, 0 as f32 };
+    let size = quadtree::Size { 1 as f32, 1 as f32 };
+    let rect = quadtree::Rect::new(origin, size);
+    let qt = quadtree::QuadTree::newAutosized(rect);
 }
--- src/quadtree/quadtree.rs
+++ src/quadtree/quadtree.rs
@@ -4,27 +4,27 @@
  Either child nodes, a single rect, or nothing.
  */
 enum Elements {
-	/// Children are top left, top right, bottom right, and bottom left, respectively.
-	Children(~QuadTree, ~QuadTree, ~QuadTree, ~QuadTree),
-	/// A single rectangle.
-	Member(Rect),
-	/// Nothing.
-	None
+    /// Children are top left, top right, bottom right, and bottom left, respectively.
+    Children(~QuadTree, ~QuadTree, ~QuadTree, ~QuadTree),
+    /// A single rectangle.
+    Member(Rect),
+    /// Nothing.
+    None
 }
 
 pub struct Point {
-	x: f32,
-	y: f32,
+    x: f32,
+    y: f32,
 }
 
 pub struct Size {
-	width: f32,
-	height: f32,
+    width: f32,
+    height: f32,
 }
 
 pub struct Rect {
-	origin: Point,
-	size: Size,
+    origin: Point,
+    size: Size,
 }
 
 /**
@@ -32,91 +32,91 @@ pub struct Rect {
  or exactly four child nodes.
  */
 pub struct QuadTree {
-	rect: Rect,
-	elements: Elements,
+    rect: Rect,
+    elements: Elements,
 }
 
 impl Rect {
-	fn new(origin: Point, size: Size) -> Rect {
-		Rect { origin: origin, size: size }
-	}
+    fn new(origin: Point, size: Size) -> Rect {
+        Rect { origin: origin, size: size }
+    }
 
-	/// Find which the rect has an origin farther to the left.
-	fn minXRect<'a>(rect1: &'a Rect, rect2: &'a Rect) -> (&'a Rect, &'a Rect) {
-		if (rect1.minX() <= rect2.minX()) {
-			(rect1, rect2)
-		} else {
-			(rect2, rect1)
-		}
-	}
+    /// Find which the rect has an origin farther to the left.
+    fn minXRect<'a>(rect1: &'a Rect, rect2: &'a Rect) -> (&'a Rect, &'a Rect) {
+        if (rect1.minX() <= rect2.minX()) {
+            (rect1, rect2)
+        } else {
+            (rect2, rect1)
+        }
+    }
 
-	/**
-	 Check if this rect entirely contains another rect.
-	 */
-	fn contains(&self, rect: &Rect) -> bool {
-		// Find which the rect has an origin farther to the left.
-		let (minXRect, otherRect) = Rect::minXRect(self, rect);
+    /**
+     Check if this rect entirely contains another rect.
+     */
+    fn contains(&self, rect: &Rect) -> bool {
+        // Find which the rect has an origin farther to the left.
+        let (minXRect, otherRect) = Rect::minXRect(self, rect);
 
-		// If the rectangles don't intersect, one cannot be contained in the other.
-		self.intersects(rect)
-		&& (minXRect.maxX() >= otherRect.maxX() && minXRect.maxY() >= otherRect.maxY())
-	}
+        // If the rectangles don't intersect, one cannot be contained in the other.
+        self.intersects(rect)
+        && (minXRect.maxX() >= otherRect.maxX() && minXRect.maxY() >= otherRect.maxY())
+    }
 
-	/**
-	 Check if this rect and another rect intersect.
-	 */
-	fn intersects(&self, rect: &Rect) -> bool {
-		let (minXRect, otherRect) = Rect::minXRect(self, rect);
+    /**
+     Check if this rect and another rect intersect.
+     */
+    fn intersects(&self, rect: &Rect) -> bool {
+        let (minXRect, otherRect) = Rect::minXRect(self, rect);
 
-		let intersects: bool = ((minXRect.maxX() >= otherRect.minX())
-			&& ((minXRect.minY() >= otherRect.minY() && minXRect.minY() <= otherRect.maxY())
-				|| (minXRect.minY() <= otherRect.minY() && minXRect.maxY() >= otherRect.minY())));
+        let intersects: bool = ((minXRect.maxX() >= otherRect.minX())
+            && ((minXRect.minY() >= otherRect.minY() && minXRect.minY() <= otherRect.maxY())
+                || (minXRect.minY() <= otherRect.minY() && minXRect.maxY() >= otherRect.minY())));
 
-		intersects
-	}
+        intersects
+    }
 
-	fn maxX(&self) -> f32 {
-		self.origin.x + self.size.width
-	}
+    fn maxX(&self) -> f32 {
+        self.origin.x + self.size.width
+    }
 
-	fn maxY(&self) -> f32 {
-		self.origin.y + self.size.height
-	}
+    fn maxY(&self) -> f32 {
+        self.origin.y + self.size.height
+    }
 
-	fn minX(&self) -> f32 {
-		self.origin.x
-	}
+    fn minX(&self) -> f32 {
+        self.origin.x
+    }
 
-	fn minY(&self) -> f32 {
-		self.origin.y
-	}
+    fn minY(&self) -> f32 {
+        self.origin.y
+    }
 }
 
 impl QuadTree {
-	/**
-	 Create a quadtree with a square root node with the same origin and size
-	 as the passed in rect.
-	 */
-	fn newAutosized(rect: Rect) -> QuadTree {
-		QuadTree { rect: rect, elements: Member(rect), }
-	}
+    /**
+     Create a quadtree with a square root node with the same origin and size
+     as the passed in rect.
+     */
+    fn newAutosized(rect: Rect) -> QuadTree {
+        QuadTree { rect: rect, elements: Member(rect), }
+    }
 
-	/**
-	 Create a quadtree with a root node with the given origin, size, and member rectangle.
-	 If `insertRect` is not entirely contained within the rectangle defined by `origin` and `size`,
-	 higher level nodes will be created to form a quadtree with a child of the rect specified
-	 and other children such that the rect is in the tree.
-	 */
-	fn new(origin: Point, size: Size, insertRect: Rect) -> QuadTree {
-		let nodeRect = Rect { origin: origin, size: size };
-		let node = QuadTree { rect: nodeRect, elements: None };
+    /**
+     Create a quadtree with a root node with the given origin, size, and member rectangle.
+     If `insertRect` is not entirely contained within the rectangle defined by `origin` and `size`,
+     higher level nodes will be created to form a quadtree with a child of the rect specified
+     and other children such that the rect is in the tree.
+     */
+    fn new(origin: Point, size: Size, insertRect: Rect) -> QuadTree {
+        let nodeRect = Rect { origin: origin, size: size };
+        let node = QuadTree { rect: nodeRect, elements: None };
 
-		node.insertRect(insertRect)
-	}
+        node.insertRect(insertRect)
+    }
 
-	fn insertRect(self, toInsert: Rect) -> QuadTree {
-		fail!("insertRect not implemented!");
+    fn insertRect(self, toInsert: Rect) -> QuadTree {
+        fail!("insertRect not implemented!");
 
-		self
-	}
+        self
+    }
 }
